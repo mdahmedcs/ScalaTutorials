3f7868c7bb0d2ff885aeb8eb66166b63b625afa3
mdahmedcs <mdahmedhydcs@gmail.com>
2020-04-26 08:49:27 -0700

    added rdd sources

--- src/com/code/scala/RDDDEMO.scala
+++ src/com/code/scala/RDDDEMO.scala
@@ -9,6 +9,7 @@ object RDDDEMO{
   val spark=SparkSession.builder().master("local[4]").appName("rdd demo").getOrCreate()
   
   val sc= spark.sparkContext
+  sc.setLogLevel("ERROR")
   
   //Method1: Parallelizing a collection
   val rdd = sc.parallelize(List(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15))
@@ -32,12 +33,36 @@ object RDDDEMO{
       
       println("First " +rdd.first())
       
+      //create rdd from seq
+      val rddseq=spark.sparkContext.parallelize(Seq(("Java", 20000), 
+  ("Python", 100000), ("Scala", 3000)))
+  
+  rddseq.collect.foreach(println)
+      
       // Method2: Reading from files: Text, CSV, 
       
-     val rdd2=sc.textFile("C:\\Users\\ahmed\\Downloads\\subscribers.csv,C:\\Users\\ahmed\\Downloads\\subscribers.csv") //reading from multiple files
+     val rdd2=sc.textFile("C:\\Users\\ahmed\\Downloads\\subscribers.csv,C:\\Users\\ahmed\\Downloads\\subscribers.csv") //reading from gitttttttmultiple files
      
      rdd2.collect().take(10).foreach(println)
      println("count: "+rdd2.count())
+     
+var rdd3 = rdd2.map(f=>f.split(","))
+
+  println("Iterate RDD")
+  rdd3.take(10).foreach(f=>{
+    println("Col1:"+f(0)+",Col2:"+f(1))
+  })
+       
+     
+  //word count example
+  val text=sc.textFile("E:\\spark_examples\\words.txt")
+  
+  text.flatMap(x=>x.split(" ")).map(y=>(y,1)).reduceByKey(_+_).collect().foreach(println)
+  
+  
+ 
+  
+     
       
       
       
