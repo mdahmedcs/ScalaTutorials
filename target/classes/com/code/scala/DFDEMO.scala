c7123c58cddf7ae97e41f4c9466f92ca74704212
mdahmedcs <mdahmedhydcs@gmail.com>
2020-04-27 23:16:29 -0700

    added rdd to df methods

--- target/classes/com/code/scala/DFDEMO.scala
+++ target/classes/com/code/scala/DFDEMO.scala
@@ -7,6 +7,7 @@ import org.apache.spark.sql.types.StructField
 import org.apache.spark.sql.types.StructType
 import org.apache.spark.sql.types.StringType
 import org.apache.spark.sql.types.IntegerType
+import org.apache.spark.sql.types.LongType
 import org.apache.spark.sql.Column
 import org.apache.spark.sql.functions.col
 import org.apache.spark.sql.functions.when
@@ -14,6 +15,8 @@ import org.apache.spark.sql.functions.min
 import org.apache.spark.sql.functions.max
 import org.apache.spark.sql.functions.sum
 import org.apache.spark.sql.functions.avg
+import org.apache.spark.sql.functions._
+
 
 object DFDEMO {
   
@@ -51,13 +54,86 @@ object DFDEMO {
   val DFFilter = DF.filter(col("id")<300)
   DFFilter.show()
  
-  //min, max
- println("min salary "+DF.select(min(col("salary"))))
- println("max salary "+DF.select(max(col("salary"))))
- println("sum salary "+DF.select(sum(col("salary"))))
- println("avg salary "+DF.select(avg(col("salary"))))
+  //min, max, sum, avg
+ DF.select(min(col("salary"))).show()
+ DF.select(max(col("salary"))).show()
+ DF.select(sum(col("salary"))).show()
+ DF.select(avg(col("salary"))).show()
+    
+ //groupBy
+ DF.groupBy("id").sum("salary").show()
+ 
+ //distinct
+ DF.select(col("salary")).distinct().show()
+
+ //sort
+ 
+ DF.sort(col("salary").desc).show()
+ DF.sort(col("salary").asc).show()
+ 
+ // joinS
+ val bonusDF=DF.withColumn("bonus", col("salary")/4).select(col("id"),col("bonus"))  
+ 
+//inner join
+ DF.join(bonusDF,DF("id") ===  bonusDF("id"),"inner").show(false)
+ 
+ //left join
+ DF.join(bonusDF,DF("id") ===  bonusDF("id"),"left").show(false)
+ 
+ //union
+ println("applying union")
+ 
+ val DFunion=DF  //union requires both data frames to have same structure
+ DF.union(DFunion).show(false)
+ 
+ println("applying union selecting distinct records")
+ DF.union(DFunion).distinct().show(false)
+ 
+ 
+ 
+ //Reading from csv using infer schema, for csv file infer schema is false by default
+ 
+   val dfcsv=spark.read.options(Map("inferschema"->"true", "header"->"true")).csv("E:\\spark_examples\\subscribers.csv")
   
+     dfcsv.show()
+     dfcsv.printSchema()
   
+ //Reading from csv by programatically specifying schema
+  
+   val schemacsv = StructType(Array(StructField("custnum", IntegerType, nullable=true),StructField("ordernum", IntegerType, nullable=true),StructField("email", StringType, nullable=true),StructField("fname", StringType, nullable=true),
+       StructField("lname", StringType, nullable=true),StructField("title", StringType, nullable=true),StructField("company", StringType, nullable=true),StructField("address", StringType, nullable=true),StructField("city", StringType, nullable=true),
+       StructField("state", StringType, nullable=true),StructField("zip", StringType, nullable=true),StructField("country", StringType, nullable=true),StructField("phone", StringType, nullable=true),StructField("brand", StringType, nullable=true)))  
+       
+   val dfsamplecsv=spark.read.format("csv")
+       .option("header","true")
+       .schema(schemacsv)
+       .load("E:\\spark_examples\\subscribers2.csv")
+   
+     dfsamplecsv.show(100,false)
+     dfsamplecsv.printSchema()
+   
+  // Redaing from JSON. for JSON infershema is true by default
+     println("reading from json using infeschema, by default true")
+     
+     val dfjson=spark.read.format("json").load("E:\\spark_examples\\subscribers.json")
+     
+     dfjson.show(10,false)
+     dfjson.printSchema()
+     
+  // Reading from JSON by programatically specifying schema
+     
+     val schemajson = StructType(Array(StructField("city", StringType, nullable=true),StructField("recnum", LongType, nullable=true),StructField("state", StringType, nullable=true),StructField("type", StringType, nullable=true),
+       StructField("zipcode", LongType, nullable=true)))
+       
+      val dfjsonsamplejson = spark.read.format("json") 
+                             .option("inferschema","false")
+                             .schema(schemajson)
+                             .load("E:\\spark_examples\\subscribers.json")
+                             
+      dfjsonsamplejson.show()
+      dfjsonsamplejson.printSchema()
+       
+    
   
   
 }
